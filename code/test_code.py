59e27b704c9bbb2a106023ab41e76e1e1f182eea
Potassium-chromate <112847018+Potassium-chromate@users.noreply.github.com>
2023-05-18 23:57:06 +0800

    Update test_code.py

--- code/test_code.py
+++ code/test_code.py
@@ -1,7 +1,8 @@
 import requests
+import json
 
-url = "http://192.168.4.1/test"  # Replace with the IP address of your ESP8266
+url = "http://192.168.4.1/sendArray"  # Replace with the IP address of your ESP8266
 array = [1, 2, 3, 4, 5]
 
-response = requests.post(url, data=str(array))
+response = requests.post(url, data=json.dumps(array))
 print(response.text)
