414aff079e3bbdd3c17f894322326b89e37911bd
Potassium-chromate <112847018+Potassium-chromate@users.noreply.github.com>
2023-06-14 20:38:14 +0800

    Update game_pad.py

--- code/game_pad.py
+++ code/game_pad.py
@@ -26,48 +26,48 @@ class move:
         if self.axe_ref[1]<-.1:
             num = abs(self.axe_ref[1])
             num = round(num,1)
-            foot.append("foward :"+str(num))
+            foot.append("forward")
         elif self.axe_ref[1]>.1:
             num = abs(self.axe_ref[1])
             num = round(num,1)
-            foot.append("backward :"+str(num))
+            foot.append("backward")
         if self.axe_ref[0]<-.1:
             num = abs(self.axe_ref[0])
             num = round(num,1)
-            foot.append("left :"+str(num))
+            foot.append("left")
         elif self.axe_ref[0]>.1:
             num = abs(self.axe_ref[0])
             num = round(num,1)
-            foot.append("right :"+str(num))  
+            foot.append("right")  
         if self.axe_ref[3]<-.1:
             num = abs(self.axe_ref[3])
             num = round(num,1)
-            arm.append("foward :"+str(num))
+            arm.append("forward")
         elif self.axe_ref[3]>.1:
             num = abs(self.axe_ref[3])
             num = round(num,1)
-            arm.append("backward :"+str(num))
+            arm.append("backward")
         if self.axe_ref[2]<-.1:
             num = abs(self.axe_ref[2])
             num = round(num,1)
-            arm.append("left :"+str(num))
+            arm.append("left")
         elif self.axe_ref[2]>.1:
             num = abs(self.axe_ref[2])
             num = round(num,1)
-            arm.append("right :"+str(num)) 
+            arm.append("right") 
             
         if self.button[0]>=0.9:
             num = abs(self.button[0])
             num = round(num,1)
-            button.append("button 0:")
+            button.append("button 0")
         elif self.button[1]>=0.9:
             num = abs(self.button[1])
             num = round(num,1)
-            button.append("button 1:")
+            button.append("button 1")
         elif self.button[2]>=0.9:
             num = abs(self.button[2])
             num = round(num,1)
-            button.append("button 2:")
+            button.append("button 2")
             
         return [foot,arm,button]
 
@@ -103,6 +103,8 @@ avg_buttons = [sum(x) / 10 for x in zip(*buttons_list)]
 print(avg_axes)
 print(avg_buttons)
 
+url = "http://192.168.4.1/sendArray"  # Replace with the IP address of your ESP8266
+
 
 # Main game loop
 while True:
@@ -125,13 +127,23 @@ while True:
     ret = agent.movement()
     
     if len(ret[0]) != 0:
-        print("foot: ",ret[0])
-    
+        print(ret[0][0])
+        if ret[0][0] == "forward":
+            response = requests.post(url, data=json.dumps([90,0,0,1,1]))
+        elif ret[0][0] == "backward":
+            response = requests.post(url, data=json.dumps([90,0,0,2,2]))
+        elif ret[0][0] == "left":
+            response = requests.post(url, data=json.dumps([90,0,0,2,1]))
+        elif ret[0][0] == "right":
+            response = requests.post(url, data=json.dumps([90,0,0,1,2]))
+        
     if len(ret[1]) != 0:
         print("arm: ",ret[1])
         
+        
     if len(ret[2]) != 0:
         print("button: ",ret[2])
+        
     
     time.sleep(0.05)
     
